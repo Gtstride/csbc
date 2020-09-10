da8b06e0801d84c0b7839e2ec7f815b3a4868457
gtstride <lewisagho47@gmail.com>
2020-09-10 12:39:48 +0100

    Install front-end starter file

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2> Welcome to the official Website of Cornerstone Baptist Church</h2>
+        <a href="https://csbcministries.org" target="_blank" rel="noopener noreferrer">
+          CSBC Website!!! Coming Soon ...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
