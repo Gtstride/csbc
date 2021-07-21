e2db7b7401ac9678fb71241b9453f158aa2c63ad
gtstride <lewisagho47@gmail.com>
2021-07-21 17:22:26 +0100

    Remove hosting errors

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import Slider from './HeaderSlider/Slider';
 
 const Home = () => {
     // const [loading, setLoading] = useState(false);
     return (
         <div>
-            {/* <Slider /> */}
-            {/* {loading ? (<div className="page-loader"></div>) : (
-                <blockquote>this is home page</blockquote>
-            )} */}
             SOmething shpould be here
             <div className="inc:_footer.html">
             </div>
