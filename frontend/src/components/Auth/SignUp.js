6388012be1a38a639ed8cc8f733a871ca098ad0c
gtstride <lewisagho47@gmail.com>
2021-07-21 13:14:32 +0100

    Home Page layout

---
+++ frontend/src/components/Auth/SignUp.js
@@ -0,0 +1,208 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+        <section
+          className="main-header"
+          style={{ backgroundImage: "url(assets/images/gallery-2.jpg)" }}
+        >
+          <header>
+            <div className="container text-center">
+              <h2 className="h2 title">Customer page</h2>
+              <ol className="breadcrumb breadcrumb-inverted">
+                <li>
+                  <a href="index.html">
+                    <span className="icon icon-home" />
+                  </a>
+                </li>
+                <li>
+                  <a className="active" href="login.html">
+                    Login &amp; Register
+                  </a>
+                </li>
+              </ol>
+            </div>
+          </header>
+        </section>
+        {/* ========================  Login & register ======================== */}
+        <section className="login-wrapper login-wrapper-page">
+          <div className="container">
+            <header className="hidden">
+              <h3 className="h3 title">Sign in</h3>
+            </header>
+            <div className="row">
+              {/* === left content === */}
+              <div className="col-md-6 col-md-offset-3">
+                {/* === login-wrapper === */}
+                <div className="login-wrapper">
+                  <div className="white-block">
+                    {/*signin*/}
+                    <div className="login-block login-block-signin">
+                      <div className="h4">
+                        Sign in{" "}
+                        <a
+                          href="/sign-in"
+                          className="btn btn-main btn-xs btn-register pull-right"
+                        >
+                          create an account
+                        </a>
+                      </div>
+                      <hr />
+                      <div className="row">
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="User ID"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <input
+                              type="password"
+                              defaultValue
+                              className="form-control"
+                              placeholder="Password"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-xs-6">
+                          <span className="checkbox">
+                            <input type="checkbox" id="checkBoxId3" />
+                            <label htmlFor="checkBoxId3">Remember me</label>
+                          </span>
+                        </div>
+                        <div className="col-xs-6 text-right">
+                          <a href="#" className="btn btn-main">
+                            Login
+                          </a>
+                        </div>
+                      </div>
+                    </div>{" "}
+                    {/*/signin*/}
+                    {/*signup*/}
+                    <div className="login-block login-block-signup">
+                      <div className="h4">
+                        Register now{" "}
+                        <a
+                          href="/register"
+                          className="btn btn-main btn-xs btn-login pull-right"
+                        >
+                          Log in
+                        </a>
+                      </div>
+                      <hr />
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="First name: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="Last name: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="Company name:"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="Zip code: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-8">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="City: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="Email: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              defaultValue
+                              className="form-control"
+                              placeholder="Phone: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <hr />
+                          <span className="checkbox">
+                            <input type="checkbox" id="checkBoxId1" />
+                            <label htmlFor="checkBoxId1">
+                              I have read and accepted the <a href="#">terms</a>, as
+                              well as read and understood our terms of{" "}
+                              <a href="#">business contidions</a>
+                            </label>
+                          </span>
+                          <span className="checkbox">
+                            <input type="checkbox" id="checkBoxId2" />
+                            <label htmlFor="checkBoxId2">
+                              Subscribe to exciting newsletters and great tips
+                            </label>
+                          </span>
+                          <hr />
+                        </div>
+                        <div className="col-md-12">
+                          <a href="#" className="btn btn-main btn-block">
+                            Create account
+                          </a>
+                        </div>
+                      </div>
+                    </div>{" "}
+                    {/*/signup*/}
+                  </div>
+                </div>{" "}
+                {/*/login-wrapper*/}
+              </div>{" "}
+              {/*/col-md-6*/}
+            </div>
+          </div>
+        </section>
+    </div>
+  )
+}
+
+export default SignUp
---
+++ frontend/src/components/Header/Slider.js
@@ -0,0 +1,141 @@
+import React from 'react';
+
+const Slider = () => {
+  return (
+    <div>
+      <section className="header-content">
+        <div className="owl-slider">
+          {/* === slide item === */}
+          <div className="item">
+            <div className="box">
+              <div className="container">
+                <h2 className="title animated h1" data-animation="fadeInDown">
+                  The Only Wise God
+                </h2>
+                <div className="animated" data-animation="fadeInUp">
+                  The &amp; one powerfull Christ. <br /> All knowing
+                  God. &amp; Knows the end from the beginning
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  <a
+                    href="https://themeforest.net/item/mobel-furniture-website-template/20382155"
+                    target="_blank" rel="noopener noreferrer"
+                    className="btn btn-main"
+                  >
+                    <i className="icon icon-phone-handset" /> Talk to Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* === slide item === */}
+          <div
+            className="item galla-2"
+          // style={{ backgroundImage: "url(assets/images/gallery-2.jpg)" }}
+          >
+            <div className="box">
+              <div className="container">
+                <h2 className="title animated h1" data-animation="fadeInDown">
+                  Experience The True God!
+                </h2>
+                <div className="animated" data-animation="fadeInUp">
+                  Worship God in Spirit &amp; In Truth
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  Furniture category icon fonts!
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  <a href="category.html" className="btn btn-clean">
+                    Get insipred
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* === slide item === */}
+          <div
+            className="item galla-3"
+          // style={{ backgroundImage: "url(assets/images/gallery-2.jpg)" }}
+          >
+            <div className="box">
+              <div className="container">
+                <h2 className="title animated h1" data-animation="fadeInDown">
+                  Worship God!
+                </h2>
+                <div className="animated" data-animation="fadeInUp">
+                  He has done &amp; is still does great things.
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  God almight
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  <a href="category.html" className="btn btn-clean">
+                   Talk to us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* === slide item === */}
+          <div
+            className="item glla-4"
+          // style={{ backgroundImage: "url(assets/images/gallery-3.jpg)" }}
+          >
+            <div className="box">
+              <div className="container">
+                <h2 className="title animated h1" data-animation="fadeInDown">
+                  Very Animate.css Friend.
+                </h2>
+                <div className="desc animated" data-animation="fadeInUp">
+                  Combine with animate.css. Or just use your own!.
+                </div>
+                <div className="desc animated" data-animation="fadeInUp">
+                  Bunch of typography effects.
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  <a
+                    href="https://themeforest.net/item/mobel-furniture-website-template/20382155"
+                    target="_blank" rel="noopener noreferrer"
+                    className="btn btn-clean"
+                  >
+                   Download Message
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* === slide item === */}
+          <div
+            className="item galla-5"
+          // style={{ backgroundImage: "url(assets/images/gallery-2.jpg)" }}
+          >
+            <div className="box">
+              <div className="container">
+                <h2 className="title animated h1" data-animation="fadeInDown">
+                  The One true Living God
+                </h2>
+                <div className="animated" data-animation="fadeInUp">
+                  No-One compares to Him
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  Get in touch
+                </div>
+                <div className="animated" data-animation="fadeInUp">
+                  <a href="category.html" className="btn btn-clean">
+                    Get insipred
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* === slide item === */}
+        </div>
+        {/*/owl-slider*/}
+      </section>
+
+    </div>
+  )
+}
+
+export default Slider
