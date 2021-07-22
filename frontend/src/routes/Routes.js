850a23b3ee49bd057b2a161d0466062a0e223ebd
gtstride <lewisagho47@gmail.com>
2021-07-22 17:02:30 +0100

    Add home route to App.js

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -3,19 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import SignIn from "../components/Auth/SignIn"
 import SignUp from '../components/Auth/SignUp';
 import ContactUs from '../components/Contact-Us/ContactUs';
-import Home from '../components/Home/Home';
+// import Home from '../components/Home/Home';
 
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
       <Route exact path="/sign-in" component={SignIn} />
       <Route exact path="/register" component={SignUp} />
       <Route exact path="/contact-us" component={ContactUs} />
 
       {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
-      <Route exact component={SignIn} />
+      <Route exact path="/sign-in" component={SignIn} />
     </Switch>
   );
 }
