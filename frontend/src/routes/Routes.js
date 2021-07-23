3dc46de296b59b20565403ce0e48ee0f1edd4494
gtstride <lewisagho47@gmail.com>
2021-07-23 13:17:47 +0100

    Make adjustment for landing page

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -3,19 +3,23 @@ import { Switch, Route } from 'react-router-dom';
 import SignIn from "../components/Auth/SignIn"
 import SignUp from '../components/Auth/SignUp';
 import ContactUs from '../components/Contact-Us/ContactUs';
-// import Home from '../components/Home/Home';
+import Home from '../components/Home/Home';
 
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route exact path="/sign-in" component={SignIn} />
-      <Route exact path="/register" component={SignUp} />
-      <Route exact path="/contact-us" component={ContactUs} />
+    <div className="wrapper">
+      <div className="page-loader"></div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/register" component={SignUp} />
+        <Route exact path="/contact-us" component={ContactUs} />
 
-      {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
-      <Route exact path="/sign-in" component={SignIn} />
-    </Switch>
+        {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
+        <Route exact path="/sign-in" component={SignIn} />
+      </Switch>
+    </div>
   );
 }
 
